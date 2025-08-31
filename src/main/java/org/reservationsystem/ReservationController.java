0789dd8ce25dddc524a82556a6d1ef4db7a2ae68
Твоё Имя <frolov.dev1375@gmail.com>
2025-09-01 00:38:52 +0700

    add ResponseEntity<T> for all restControllers

--- src/main/java/org/reservationsystem/ReservationController.java
+++ src/main/java/org/reservationsystem/ReservationController.java
@@ -3,10 +3,15 @@ package org.reservationsystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+
+import static org.reservationsystem.CustomColoriOutput.ANSI_CYAN;
+import static org.reservationsystem.CustomColoriOutput.ANSI_RESET;
 
 @RestController
 public class ReservationController {
@@ -14,27 +19,29 @@ public class ReservationController {
     private final ReservationService reservationService;
 
     @Autowired
-    public ReservationController(
-            ReservationService reservationService) {
+    public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
     }
 
     @GetMapping("/{id}")
-    public Optional<Reservation> getReservationByID(
-            @PathVariable("id") Long id) {                             // Получение ID через GET запрос
-        logger.info("Вызвался метод getReservationByID с ID {}", id);
-        return reservationService.getReservationByID(id);
+    public ResponseEntity<Optional<Reservation>> getReservationByID(
+            @PathVariable("id") Long id) {
+        logger.info(ANSI_CYAN + "Вызвался метод getReservationByID с ID : {}" + ANSI_RESET, id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(reservationService.getReservationByID(id));
     }
 
     @GetMapping("/all")
-    public List<Reservation> getAllReservations() {
-        logger.info("Вызвался метод getAllReservations");
-        return reservationService.getAllReservation();
+    public ResponseEntity<List<Reservation>> getAllReservations() {
+        logger.info(ANSI_CYAN + "Вызвался метод getAllReservations" + ANSI_RESET);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(reservationService.getAllReservation());
     }
 
     @PostMapping()
-    public Reservation createReservation(@RequestBody Reservation newReservation) {
-        logger.info("Создание нового объекта Reservation");
-        return reservationService.createReservation(newReservation);
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation newReservation) {
+        logger.info(ANSI_CYAN + "Создание нового объекта Reservation" + ANSI_RESET);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(reservationService.createReservation(newReservation));
     }
 }
